3e390750f928c8b0332f1b0c0e41ab1d32dd192c
LePhamThuyDuyen <lephamthuyduyen2704@gmail.com>
2021-04-02 12:00:01 +0700

    Add custom Logout page

---
+++ MyProject_Backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyProject_Backend.Data;
+using MyProject_Backend.Models;
+
+[assembly: HostingStartup(typeof(MyProject_Backend.Areas.Identity.IdentityHostingStartup))]
+namespace MyProject_Backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

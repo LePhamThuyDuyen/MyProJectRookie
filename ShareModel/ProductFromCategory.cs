da923dbb98e5040637b02975456743e8b69f54d0
MSI\LeDuyen <lephamthuyduyen2704@gmail.com>
2021-04-05 16:32:46 +0700

    add ProductromCategory in ShareModel

---
+++ ShareModel/ProductFromCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareModel
+{
+    public class ProductFromCategory
+    {
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}

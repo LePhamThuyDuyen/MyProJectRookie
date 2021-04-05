273155b1a4c9a78b372f77f0a8de835ade19ac32
MSI\LeDuyen <lephamthuyduyen2704@gmail.com>
2021-04-05 14:15:42 +0700

    add Get product in ProductController

---
+++ MyProject_Backend/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyProject_Backend.Data;
+using ShareModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ProductController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ProductShare>> Get()
+        {
+            var products = await _applicationDbContext.products.Select(x => new ProductShare
+            {
+                ProductID = x.Id,
+                ProductName = x.Name,
+                Price = x.Price,
+                Description = x.Description,
+               // Image = x.Image,
+
+            }).ToListAsync();
+            return Ok(products);
+        }
+
+    }
+}

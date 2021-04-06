cf145c00d8efb2f6705afc7fbdf26a651f60e2e4
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-06 14:02:12 +0700

    Create Interface Category

---
+++ SerVice/InterfaceService/ICategory.cs
@@ -0,0 +1,22 @@
+﻿using MyProject_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerVice.InterfaceService
+{
+  public  interface ICategory
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+
+      
+
+        Task<Category> CreateAsync(Category cate);
+
+        Task<Category> UpdateAsync(int id, Category cate);
+
+        Task<Category> DeleteAsync(int id);
+    }
+}

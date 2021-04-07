83596c5b040f0a9c50b17d39b8411f0b557ebb91
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-07 23:57:44 +0700

    update Interface product

--- MyProject_Backend/InterfaceService/IProduct.cs
+++ MyProject_Backend/InterfaceService/IProduct.cs
@@ -1,21 +1,19 @@
 ﻿using MyProject_Backend.Models;
-using System;
+using ShareModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyProject_Backend.Controllers
 {
-  public  interface IProduct
+    public  interface IProduct
     {
         Task<Product> CreateAsync(Product pro);
 
         Task<Product> DeleteAsync(int id);
 
-        Task<IEnumerable<Product>> GetAllAsync();
+        Task<IEnumerable<ProductShare>> GetAllAsync();
 
-        Task<IEnumerable<Product>> GetByCategoryAsync(string categoryName);
+        Task<IEnumerable<ProductShare>> GetByCategoryAsync(string categoryName);
 
         Task<Product> UpdateAsync(int id, Product pro);
     }

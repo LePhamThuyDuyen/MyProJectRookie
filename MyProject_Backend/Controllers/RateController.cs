5f8ce95404104d7117c8a93b35656f0ea06d71a8
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-12 17:53:00 +0700

    Create Api Rate

---
+++ MyProject_Backend/Controllers/RateController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyProject_Backend.InterfaceService;
+using ShareModel;
+using System.Threading.Tasks;
+
+namespace MyProject_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RateController : ControllerBase
+    {
+        private readonly IRate _rate;
+       
+
+        public RateController(IRate rate)
+        {
+            _rate = rate;
+          
+        }
+
+        [HttpPost]
+         [Authorize("Bearer")]
+        //[Authorize]
+        public async Task<IActionResult> CreateRate(RateShare rateShare)
+        {
+            var result = await _rate.CreateRate(rateShare);
+            return Ok(result);
+        }
+    }
+}

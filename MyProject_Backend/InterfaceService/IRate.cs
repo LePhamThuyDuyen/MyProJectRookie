c2535ee0917c1941e9eb45177b16bffc721a4a2b
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-12 17:52:28 +0700

    Create sevice Rate

---
+++ MyProject_Backend/InterfaceService/IRate.cs
@@ -0,0 +1,16 @@
+﻿using MyProject_Backend.Models;
+using ShareModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject_Backend.InterfaceService
+{
+    public interface IRate
+    {
+       
+        Task<bool> CreateRate (RateShare rate);
+
+    }
+}
---
+++ MyProject_Backend/Service/RateService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using MyProject_Backend.Data;
+using MyProject_Backend.InterfaceService;
+using MyProject_Backend.Models;
+using ShareModel;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MyProject_Backend.Service
+{
+    public class RateService: IRate
+    {
+        private readonly ApplicationDbContext _applicationDb;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public RateService (ApplicationDbContext applicationDb, IHttpContextAccessor httpContextAccessor)
+        {
+            _applicationDb = applicationDb;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+
+        public async  Task<bool> CreateRate(RateShare rateShare)
+        {
+
+            var rates = new Rate { ProductId = rateShare.ProductId, Value = rateShare.value};
+
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            rates.UserId = userId.ToString();
+            _applicationDb.Add(rates);
+            await _applicationDb.SaveChangesAsync();
+            return true;
+
+        }
+
+    }
+}

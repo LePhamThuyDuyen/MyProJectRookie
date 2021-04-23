c6b3a9dd133a5486aa74626d58af4c32008c68f8
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 13:32:49 +0700

    Add history  to redirect  list category

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,16 +1,18 @@
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import CategoryForm from './Pages/Categoties/CategoryForm';
-import { BrowserRouter ,Switch, Route } from "react-router-dom";
+import { Router ,Switch, Route } from "react-router-dom";
 import ListCategory from './Pages/Categoties/Listcategory';
-import { LIST_CATEGORY,CREATE_CATEGORY } from './components/router';
+import { LIST_CATEGORY,CREATE_CATEGORY, UPDATE_CATEGORY} from './components/router';
+import history from'./Helpers/help';
 
 function App() {
   return (
+    
     <div className="App">
       <Navbar />
         <Header />
-      <BrowserRouter>
+        <Router history={history}>
       <Switch>
         <Route path={LIST_CATEGORY}>
           <ListCategory />
@@ -18,12 +20,11 @@ function App() {
         <Route path={CREATE_CATEGORY}>
           <CategoryForm />
         </Route>
+        <Route path={UPDATE_CATEGORY}>
+          <CategoryForm />
+        </Route>
       </Switch>
-    </BrowserRouter>
-     
-        
-
-
+    </Router>
     </div>
   );
 }

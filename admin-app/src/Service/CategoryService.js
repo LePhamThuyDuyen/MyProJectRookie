65bbedcb01cfcc951e48d37cb61747c253b55fa4
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 09:40:43 +0700

    Update service category

--- admin-app/src/Service/CategoryService.js
+++ admin-app/src/Service/CategoryService.js
@@ -6,9 +6,14 @@ class CategoryService {
   getList() {
     return httpClient.get(this.pathSer);
   }
+
+  get(id){
+    return httpClient.get(this.pathSer+"/"+id);
+  }
   edit(id, objectEdit) {
     return httpClient.put(this.pathSer + "/" + id, objectEdit);
   }
+
 
   delete(id) {
     return httpClient.delete(this.pathSer + "/" + id);  

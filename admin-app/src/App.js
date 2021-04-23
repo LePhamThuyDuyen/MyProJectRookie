5010bfcb138247397ffdc750e0f7b255741a3eee
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 09:41:13 +0700

    update app.js admin

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,15 +1,29 @@
-
-import Header from './Pages/Header';
-import Navbar from './Pages/Navbar';
-import Content from './Pages/Content';
-
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import CategoryForm from './Pages/Categoties/CategoryForm';
+import { BrowserRouter ,Switch, Route } from "react-router-dom";
+import ListCategory from './Pages/Categoties/Listcategory';
+import { LIST_CATEGORY,CREATE_CATEGORY } from './components/router';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-    <Header/>
-    <Content/>
+      <Navbar />
+        <Header />
+      <BrowserRouter>
+      <Switch>
+        <Route path={LIST_CATEGORY}>
+          <ListCategory />
+        </Route>
+        <Route path={CREATE_CATEGORY}>
+          <CategoryForm />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+     
+        
+
+
     </div>
   );
 }

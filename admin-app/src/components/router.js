48d229c1089831cdcf4ffff12c52906cb51e9803
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 09:38:30 +0700

    Update router admin

---
+++ admin-app/src/components/router.js
@@ -0,0 +1,7 @@
+export const LIST_PRODUCT = 'product';
+export const CREATE_PRODUCT = 'product';
+export const UPDATE_PRODUCT = 'product';
+
+export const LIST_CATEGORY = '/category';
+export const CREATE_CATEGORY = '/createcategory';
+export const UPDATE_CATEGORY = '/Editcategory/:id';

ca5148b288d97f93f4789376d87c179fcd423cb2
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 14:43:39 +0700

    update Product Controller backend

--- MyProject_Backend/Controllers/ProductController.cs
+++ MyProject_Backend/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyProject_Backend.Data;
+using MyProject_Backend.Models;
 using ShareModel;
 using System.Linq;
 using System.Threading.Tasks;
@@ -44,7 +45,41 @@ namespace MyProject_Backend.Controllers
             return Ok(results);
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Create( ProductShare productShare)
+        {
+            Product pro = new Product();
+            pro.Name = productShare.ProductName;
+            pro.Description = productShare.Description;
+            pro.CategoryId = productShare.CategoryID;
+            pro.Price = productShare.Price;
+            pro.Image = productShare.Image;
+            var result = await _product.CreateAsync(pro);
+            return Ok(result);
+        }
 
+        [HttpDelete]
+        [AllowAnonymous]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _product.DeleteAsync(id);
+            if (result == null)
+                return NotFound();
+            return Ok(result);
+        }
 
-    }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult> Update(int id, [FromForm] ProductShare model)
+        {
+            var product = await _product.FindByIdAsync(id);
+            product.Name = model.ProductName;
+            product.Description = model.Description;
+            product.Price = model.Price;
+            product.CategoryId = model.CategoryID   ;
+            var result = await _product.UpdateAsync(id, product);
+            return Ok(result);
+        }
+        }
 }

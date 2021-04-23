a1ae689b9dc10314871511b23807f4d37427f2e3
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 13:28:03 +0700

    Add react-history admin

--- admin-app/src/components/router.js
+++ admin-app/src/components/router.js
@@ -1,6 +1,6 @@
-export const LIST_PRODUCT = 'product';
-export const CREATE_PRODUCT = 'product';
-export const UPDATE_PRODUCT = 'product';
+export const LIST_PRODUCT = '/product';
+export const CREATE_PRODUCT = '/product';
+export const UPDATE_PRODUCT = '/product';
 
 export const LIST_CATEGORY = '/category';
 export const CREATE_CATEGORY = '/createcategory';

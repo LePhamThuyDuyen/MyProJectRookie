6e26adbf43f84c765a355f0c3d2a0d9636d6a414
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-23 16:48:33 +0700

    Update App.js admin

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -3,8 +3,10 @@ import Navbar from './components/Navbar';
 import CategoryForm from './Pages/Categoties/CategoryForm';
 import { Router ,Switch, Route } from "react-router-dom";
 import ListCategory from './Pages/Categoties/Listcategory';
-import { LIST_CATEGORY,CREATE_CATEGORY, UPDATE_CATEGORY} from './components/router';
+import { LIST_CATEGORY,CREATE_CATEGORY, UPDATE_CATEGORY,LIST_USER, LIST_PRODUCT} from './components/router';
 import history from'./Helpers/help';
+import ListUser from './Pages/User/ListUser';
+import ListProduct from'./Pages/Products/ListProduct';
 
 function App() {
   return (
@@ -22,6 +24,12 @@ function App() {
         </Route>
         <Route path={UPDATE_CATEGORY}>
           <CategoryForm />
+        </Route>
+        <Route path={LIST_USER}>
+          <ListUser />
+        </Route>
+        <Route path={LIST_PRODUCT}>
+          <ListProduct />
         </Route>
       </Switch>
     </Router>

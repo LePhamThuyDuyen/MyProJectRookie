5c725cbb1f55f685139362ee6a928292c7834afa
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-25 19:10:29 +0700

    Add ProductCreateRequest in share model

---
+++ ShareModel/ProductCreateRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+
+namespace ShareModel
+{
+    public  class ProductCreateRequest
+    {
+        //public int ProductID { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public IFormFile ImageRequest { get; set; }
+    }
+}

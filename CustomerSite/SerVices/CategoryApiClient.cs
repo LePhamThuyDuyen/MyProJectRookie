a6f076d987364c01048b87ffda19cc8e0b4fa734
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-04-28 11:17:15 +0700

    refactor  service customer Site

--- CustomerSite/SerVices/CategoryApiClient.cs
+++ CustomerSite/SerVices/CategoryApiClient.cs
@@ -1,4 +1,5 @@
-﻿using ShareModel;
+﻿using Microsoft.Extensions.Configuration;
+using ShareModel;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -8,16 +9,18 @@ namespace CustomerSite.SerVices
     public class CategoryApiClient : ICategoryApiClient
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
 
-        public CategoryApiClient (IHttpClientFactory httpClientFactory)
+        public CategoryApiClient (IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
 
         }
         public async Task<IList<CategoryShare>> GetCategories()
         {
-            var client = _httpClientFactory.CreateClient("myapi");
-            var response = await client.GetAsync("/api/Category");
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync(_configuration.GetValue<string>("Backend") + "api/Category");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<IList<CategoryShare>>();
         }
--- CustomerSite/SerVices/ProductApiClient.cs
+++ CustomerSite/SerVices/ProductApiClient.cs
@@ -27,24 +27,24 @@ namespace CustomerSite.SerVices
         }
         public async Task<IList<ProductShare>> GetProducts()
         {
-            var client = _httpClientFactory.CreateClient("myapi");
-            var response = await client.GetAsync("/Product");
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync(_configuration.GetValue<string>("Backend") + "Product");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<IList<ProductShare>>();
         }
 
         public async Task<ProductShare> GetProductById(int id)
         {
-            var client = _httpClientFactory.CreateClient("myapi");
-            var response = await client.GetAsync("/Product/"+id);
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync(_configuration.GetValue<string>("Backend") + "Product/" +id.ToString());
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<ProductShare>();
         }
 
         public async Task<IList<ProductFromCategory>> GetProductByCategory(string categoryName)
         {
-            var client = _httpClientFactory.CreateClient("myapi");
-            var response = await client.GetAsync("/Product/category="+ categoryName);
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync(_configuration.GetValue<string>("Backend") + "Product/category=" + categoryName);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<IList<ProductFromCategory>>();
         }
@@ -52,17 +52,17 @@ namespace CustomerSite.SerVices
         public async  Task<bool> Rating(int productId, int values)
         {
             var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
-            var client = _httpClientFactory.CreateClient("myapi");
+            var client = _httpClientFactory.CreateClient();
             client.UseBearerToken(accessToken);
             var rateRequest = new RateShare
             {
                 ProductId = productId,
                 value = values
-            };
+            };  
             var json = JsonConvert.SerializeObject(rateRequest);
             var data = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var res = await client.PostAsync("/api/Rate" ,data);
+            var res = await client.PostAsync(_configuration.GetValue<string>("Backend") + "api/Rate", data);
 
             res.EnsureSuccessStatusCode();
 

44afcc0fe46b14c4d7f43d6a02287cdd784aa942
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-04 15:17:30 +0700

    Create cart controller fontend

---
+++ CustomerSite/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using CustomerSite.SerVices;
+using Microsoft.AspNetCore.Mvc;
+using ShareModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Controllers
+{
+    public class CartController : Controller
+    {
+        [Route("/cart")]
+        public IActionResult Index()
+        {
+            var result = HttpContext.Session.Get<List<CartItemShare>>("UserCart");
+            if (result == null)
+            {
+                result = new List<CartItemShare>();
+            }
+            return View(result);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            List<CartItemShare> cart = HttpContext.Session.Get<List<CartItemShare>>("UserCart");
+
+            var deleteMe = cart.Find(x => x.ProductId == id);
+
+            cart.Remove(deleteMe);
+            HttpContext.Session.Set("UserCart", cart);
+            Task.WaitAll(Task.Delay(2000));
+            return RedirectToAction("Index", "Cart");
+        }
+
+    }
+}
--- CustomerSite/Controllers/ProductController.cs
+++ CustomerSite/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using CustomerSite.SerVices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShareModel;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CustomerSite.Controllers
@@ -25,6 +28,42 @@ namespace CustomerSite.Controllers
         {
             var results = await _productApiClient.GetProductByCategory(categoryName);
             return View(results);
+        }
+
+        public async Task<IActionResult> addProductToCart(int id, int qty)
+        {
+            List<CartItemShare> cart = HttpContext.Session.Get<List<CartItemShare>>("UserCart");
+            if (cart == null)
+            {
+                cart = new List<CartItemShare>();
+            }
+
+            foreach (var item in cart)
+            {
+                if (item.ProductId == id)
+                {
+                    item.Quality += qty;
+                    HttpContext.Session.Set<List<CartItemShare>>("UserCart", cart);
+                    Task.WaitAll(Task.Delay(2000));
+                    return RedirectToAction("Details", "Product", new { id = id });
+                }
+            }
+
+            var product = await _productApiClient.GetProductById(id);
+            var productItem = new CartItemShare
+            {
+                ProductId = (int)product.ProductID,
+                ProductName = product.ProductName,
+                Price = (decimal)product.Price,
+                Quality = qty,
+                Image = product.Image
+            };
+
+            cart.Add(productItem);
+            HttpContext.Session.Set<List<CartItemShare>>("UserCart", cart);
+
+            Task.WaitAll(Task.Delay(2000));
+            return RedirectToAction("Details", "Product", new { id = id });
         }
 
     }

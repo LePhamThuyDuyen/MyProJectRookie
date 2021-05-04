36c0a4c47bc4cc8dcef2aa85e16d3a8a56f77993
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-04 15:16:32 +0700

    Add share model cart

---
+++ ShareModel/CartItemShare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareModel
+{
+   public class CartItemShare
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quality { get; set; }
+
+        public string Image { get; set; }
+    }
+}
